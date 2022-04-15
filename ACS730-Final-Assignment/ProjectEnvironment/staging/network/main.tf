12dfc7ed10eb95d8870d6224af32d568994e6d69
shajaini <ssasikumar-vijaya-ni@myseneca.ca>
2022-04-15 18:21:11 +0000

    Staging network changes

--- ACS730-Final-Assignment/ProjectEnvironment/staging/network/main.tf
+++ ACS730-Final-Assignment/ProjectEnvironment/staging/network/main.tf
@@ -1,10 +1,10 @@
 # Module to deploy basic networking 
-module "vpc-prod" {
+module "vpc-staging" {
   source = "../../../modules/aws_network"
   #source              = "git@github.com:igeiman/aws_network.git"
   env                 = var.env
   vpc_cidr            = var.vpc_cidr
-  public_cidr_blocks = var.public_subnet_cidrs
+  public_cidr_blocks  = var.public_subnet_cidrs
   private_cidr_blocks = var.private_cidr_blocks
   prefix              = var.prefix
   default_tags        = var.default_tags
---
+++ ACS730-Final-Assignment/ProjectEnvironment/staging/network/variables.tf
@@ -0,0 +1,51 @@
+# Default tags
+variable "default_tags" {
+  default = {
+    "Owner" = "Shajaini",
+    "App"   = "Web"
+  }
+  type        = map(any)
+  description = "Default tags to be appliad to all AWS resources"
+}
+
+# Name prefix
+variable "prefix" {
+  type        = string
+  default     = "final"
+  description = "Name prefix"
+}
+
+# Provision public subnets in custom VPC
+variable "public_subnet_cidrs" {
+  default     = ["10.200.0.0/24", "10.200.1.0/24", "10.200.2.0/24"]
+  type        = list(string)
+  description = "Public Subnet CIDRs"
+}
+
+# Provision private subnets in custom VPC
+variable "private_cidr_blocks" {
+  default     = ["10.200.3.0/24", "10.200.4.0/24", "10.200.5.0/24"]
+  type        = list(string)
+  description = "Private Subnet CIDRs"
+}
+
+# VPC CIDR range
+variable "vpc_cidr" {
+  default     = "10.200.0.0/16"
+  type        = string
+  description = "VPC to host static web site"
+}
+
+# Variable to signal the current environment 
+variable "env" {
+  default     = "staging"
+  type        = string
+  description = "Deployment Environment"
+}
+
+#Availability Zones
+variable "availability_zone" {
+  default     = ["us-east-1b", "us-east-1c", "us-east-1d"]
+  type        = list(string)
+  description = "AWS regions"
+}
